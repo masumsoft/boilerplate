793394d875d4e2a279b72059736c027d296fefad
Masum <masumsofts@yahoo.com>
2017-05-05 19:34:15 +0600

    react, babel & webpack module updates

--- src/theme/buildExtractStylesLoader.js
+++ src/theme/buildExtractStylesLoader.js
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 /**
  * @summary: The encoder function is capable to apply either incoming strings from webpack modules or objects.
  * @param loader
  * @returns {string}
  */
+=======
+>>>>>>> react, babel & webpack module updates
 function encodeLoader(loader) {
   if (typeof loader === 'string') {
     return loader;
@@ -11,24 +14,35 @@ function encodeLoader(loader) {
   if (typeof loader.options !== 'undefined') {
     const query = Object
       .keys(loader.options)
+<<<<<<< HEAD
       .map(param => `${encodeURIComponent(param)}=${encodeURIComponent(loader.options[param])}`)
+=======
+      .map((param) => `${encodeURIComponent(param)}=${encodeURIComponent(loader.options[param])}`)
+>>>>>>> react, babel & webpack module updates
       .join('&');
     return `${loader.loader}?${query}`;
   }
   return loader.loader;
 }
 
+<<<<<<< HEAD
 /**
  * buildExtractStylesLoader can also deal with options without any trouble as
  * it converts them to query parameters if needed.
  */
+=======
+>>>>>>> react, babel & webpack module updates
 module.exports = function buildExtractStylesLoader(loaders) {
   const extractTextLoader = encodeLoader(loaders[0]);
   const fallbackLoader = encodeLoader(loaders[1]);
 
   const restLoaders = loaders
     .slice(2)
+<<<<<<< HEAD
     .map(loader => {
+=======
+    .map((loader) => {
+>>>>>>> react, babel & webpack module updates
       if (typeof loader === 'string') {
         return loader;
       }
@@ -37,6 +51,12 @@ module.exports = function buildExtractStylesLoader(loaders) {
 
   return [
     extractTextLoader,
+<<<<<<< HEAD
     fallbackLoader
   ].concat(restLoaders).join('!');
+=======
+    fallbackLoader,
+    ...restLoaders,
+  ].join('!');
+>>>>>>> react, babel & webpack module updates
 };
--- src/theme/font-awesome.config.prod.js
+++ src/theme/font-awesome.config.prod.js
@@ -1,13 +1,24 @@
 const buildExtractStylesLoader = require('./buildExtractStylesLoader');
 const fontAwesomeConfig = require('./font-awesome.config.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-/**
- * buildExtractStylesLoader can also deal with options without any trouble as
- * it converts them to query parameters if needed.
- */
 fontAwesomeConfig.styleLoader = buildExtractStylesLoader(ExtractTextPlugin.extract({
   fallback: 'style-loader',
-  use: ['css-loader', 'less-loader'],
+  use: [
+    {
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        importLoaders: 3,
+        sourceMap: true
+      }
+    }, {
+      loader: 'less-loader',
+      query: {
+        outputStyle: 'expanded',
+        sourceMap: true,
+        sourceMapContents: true
+      }
+    }
+  ]
 }));
 module.exports = fontAwesomeConfig;
